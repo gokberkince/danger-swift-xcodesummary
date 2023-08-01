3afdae9e2f222ec0b9ace65d66b600ab311e0def
gokberkince <53167930+gokberkince@users.noreply.github.com>
2023-08-01 17:11:24 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,9 +13,9 @@ let package = Package(
         .library(name: "DangerDeps", type: .dynamic, targets: ["DangerDependencies"]) // dev
     ],
     dependencies: [
-        .package(url: "https://github.com/danger/swift", from: "3.0.0"),
+        .package(url: "https://github.com/danger/swift", from: "3.1.17"),
         .package(url: "https://github.com/f-meloni/Rocket", from: "1.0.0"), // dev,
-        .package(url: "https://github.com/f-meloni/danger-swift-coverage", from: "1.0.0"), // dev
+        .package(url: "https://github.com/gokberkince/danger-swift-coverage", from: "1.0.0"), // dev
     ],
     targets: [
         .target(name: "DangerDependencies",dependencies: ["Danger", "DangerXCodeSummary", "DangerSwiftCoverage"]), //dev
